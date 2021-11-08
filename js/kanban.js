92c584e2c0b55dce3937a3b2a6ffb26728e90b5d
Kaml Romodan <Kaml.Romodan@gmail.com>
2021-11-08 08:08:15 +0300

    Done.... With DataTransfer errors.

--- js/kanban.js
+++ js/kanban.js
@@ -1,12 +1,12 @@
 let order = 1;
 let adding = false;
 
-const error = document.querySelector('.error');
-const message = 'Please add a description.';
+const error = document.querySelector(".error");
+const message = "Please add a description.";
 
-const add_btn = document.querySelector('.add');
-add_btn.addEventListener('click', () => {
-  const target = document.querySelector('#requested');
+const add_btn = document.querySelector(".add");
+add_btn.addEventListener("click", () => {
+  const target = document.querySelector("#requested");
   if (adding == false) {
     adding = true;
     target.appendChild(create_item());
@@ -15,8 +15,46 @@ add_btn.addEventListener('click', () => {
   }
 });
 
-const create_item = () => {
-};
+function create_item() {
+  const item = document.createElement("div");
+  item.classList.add("item");
+  item.id = `item-${order}`;
+  item.setAttribute("draggable", "true");
 
-document.querySelectorAll('.drop').forEach(element => {
+  item.addEventListener("dragstart", (e) => {
+    e.Datatransfer.setData("text", `${e.target.id}`);
+  });
+
+  item.addEventListener("dragend", (e) => {
+    e.Datatransfer.clearData();
+  });
+
+  const input = document.createElement("input");
+  item.appendChild(input);
+
+  const save_btn = document.createElement("button");
+  save_btn.innerHTML = "Save";
+  save_btn.addEventListener("click", () => {
+    error.innerHTML = "";
+    if (input.value != "") {
+      order += 1;
+      item.innerHTML = input.value;
+      adding = false;
+    } else error = message;
+  });
+  item.appendChild(save_btn);
+
+  return item;
+}
+
+document.querySelectorAll(".drop").forEach((element) => {
+  element.addEventListener("drop", (e) => {
+    e.preventDefault();
+    const id = e.Datatransfer.getData("text");
+    e.target.appendChild(document.getElementById(id));
+  });
+
+  element.addEventListener("dragover", (e) => {
+    e.preventDefault()
+  })
 });
